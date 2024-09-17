1856ce7384fd20024abdbc189e11771430e60c4a
Elisp12 <eliseuspdam.1999@gamil.com>
2024-09-17 19:36:43 -0300

    inspectdb em banco de dados

--- filtros/models.py
+++ filtros/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Produtos(models.Model):
+    nome = models.CharField(max_length=90)
+    descricao = models.TextField(blank=True, null=True)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    id_categoria = models.IntegerField(blank=True, null=True)
+    no_categoria = models.CharField(blank=True, null=True)
+    no_loja = models.CharField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'produtos'
+class ListaProduto(models.Model):
+    nome = models.CharField()
+    descricao = models.TextField(blank=True, null=True)
+    preco = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    is_ativo = models.BooleanField(blank=True, null=True)
+    quantidade = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'lista_produto'
