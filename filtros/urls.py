836d9d3c091c50168959d574aaa0b5b4360aad7c
Elisp12 <eliseuspdam.1999@gamil.com>
2024-09-17 19:48:10 -0300

    listando tabelas

--- filtros/urls.py
+++ filtros/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from filtros.views.filtrando import *
+
 urlpatterns = [
+    path('', lista_tabelas, name= 'lista_tabelas'),
 ]
--- filtros/views/filtrando.py
+++ filtros/views/filtrando.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
 
+from filtros.models import Produtos, ListaProduto
+
+def lista_tabelas(request):
+    produto = Produtos.objects.all()
+    lista_produto = ListaProduto.objects.all()
+    
+    context = {
+        
+    }
+    return render(request, 'index.html', context = context)
