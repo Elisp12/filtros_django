8283e1fe248f59771454c58c13a6737fd8a1fbb4
Elisp12 <eliseuspdam.1999@gamil.com>
2024-09-17 20:14:41 -0300

    atualiza dados da tabela, filtrado por dados iguais entre duas tabelas

--- filtros/views/filtrando.py
+++ filtros/views/filtrando.py
@@ -6,6 +6,19 @@ def lista_tabelas(request):
     produto = Produtos.objects.all()
     lista_produto = ListaProduto.objects.all()
     
+    for p in produto:
+        new_nome = p.nome
+        new_descricao = p.descricao
+        new_preco = p.preco
+        new_quantidade = p.quantidade
+        
+        lista_produto.filter(nome = p.nome).update(
+            nome = new_nome,
+            descricao = new_descricao,
+            preco = new_preco,
+            quantidade = new_quantidade
+        )    
+        
     context = {
         
     }
